e77b2fd3b43539960fc39c356c97f5c9c2eb8ee4
Tuku <tukuwebian@gmail.com>
2025-12-04 00:15:32 +0300

    book card added by api

--- src/assets/Pages/Book/Book.jsx
+++ src/assets/Pages/Book/Book.jsx
@@ -1,23 +1,28 @@
 import React from "react";
+import { RiStarLine } from "react-icons/ri";
 
-const Book = () => {
+const Book = ({ book }) => {
+  const { image, tags, bookName, author, category,rating } = book;
   return (
     <div className="card bg-base-100 shadow-sm">
-      <figure className="px-3 pt-3">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
-          className="rounded-xl"
-        />
+      <figure className="p-8 bg-base-200 m-3 rounded-b-lg">
+        <img src={image} alt="Shoes" className="rounded-xl h-36" />
       </figure>
-      <div className="card-body items-center text-center">
-        <h2 className="card-title">Card Title</h2>
-        <p>
-          A card component has a figure, a body part, and inside body there are
-          title and actions parts
-        </p>
-        <div className="card-actions">
-          <button className="btn btn-primary">Buy Now</button>
+      <div className="card-body">
+        <div className="flex gap-2">
+          <h2 className="bg-green-50 px-4 rounded-full text-green-700 font-semibold">
+            {tags[0]}
+          </h2>
+          <h2 className="bg-green-50 px-4 rounded-full text-green-700 font-semibold">
+            {tags[1]}
+          </h2>
+        </div>
+        <h1 className="card-title">{bookName}</h1>
+        <p>By: {author}</p>
+        <hr className="border border-dashed border-base-300" />
+        <div className="flex">
+          <p>{category}</p>
+          <p className="flex items-center">{rating} <RiStarLine /> </p>
         </div>
       </div>
     </div>
